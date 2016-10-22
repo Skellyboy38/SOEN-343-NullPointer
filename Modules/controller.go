92f81a476f2f99949f84c827fd7390d7e95b31bb
George_Kolampas <g.kolampas@gmail.com>
2016-10-21 21:05:18 -0400

    removed some things

--- Modules/controller.go
+++ Modules/controller.go
@@ -3,21 +3,18 @@ package main
 import (
 	"fmt"
 	"github.com/Skellyboy38/SOEN-343-NullPointer/Modules/dB"
-	"github.com/Skellyboy38/SOEN-343-NullPointer/Modules/mapper"
+	// "github.com/Skellyboy38/SOEN-343-NullPointer/Modules/mappers"
 	//	 	"github.com/Skellyboy38/SOEN-343-NullPointer/Modules/handler"
 	"log"
 	"net/http"
 )
 
-type Ball struct {
-	name string
-	size int
-}
-
 func main() {
 	fmt.Println("Application started")
-	ball := Ball{"puma", 12}
-	mapper.Insert(&ball)
+
 	dB.Init("postgres", "user=soen343 password=soen343 sslmode=disable dbname=registry")
+	defer dB.Db.Close()
+
 	log.Fatal(http.ListenAndServe(":9000", nil))
+
 }
