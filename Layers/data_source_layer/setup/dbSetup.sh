d84366cd2d1712547e21c80c72c2ab7a65ee2ae5
MGiver- <gkolampas@gmail.com>
2016-10-24 15:44:30 -0400

    crete db setup refined

--- Layers/data_source_layer/setup/dbSetup.sh
+++ Layers/data_source_layer/setup/dbSetup.sh
@@ -1,3 +1,5 @@
+initdb registry
 psql -U postgres -c 'CREATE DATABASE registry'
 psql -U postgres -d registry -c "CREATE USER soen343 PASSWORD 'soen343'"
 psql -U postgres -d registry -c 'GRANT ALL ON DATABASE registry TO soen343'
+pg_ctl.exe -start -D registry -l logfile
