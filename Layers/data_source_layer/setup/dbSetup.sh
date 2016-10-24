269a6fcb3e0897aa7723602ae27fa7a60fd4ad7a
MGiver- <gkolampas@gmail.com>
2016-10-24 15:58:09 -0400

    change -start to start in dbsetup

--- Layers/data_source_layer/setup/dbSetup.sh
+++ Layers/data_source_layer/setup/dbSetup.sh
@@ -2,4 +2,4 @@ initdb registry
 psql -U postgres -c 'CREATE DATABASE registry'
 psql -U postgres -d registry -c "CREATE USER soen343 PASSWORD 'soen343'"
 psql -U postgres -d registry -c 'GRANT ALL ON DATABASE registry TO soen343'
-pg_ctl.exe -start -D registry -l logfile
+pg_ctl.exe start -D registry -l logfile
