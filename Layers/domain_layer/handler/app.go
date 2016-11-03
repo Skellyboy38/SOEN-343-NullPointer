fceda5152693e33b73348b5d5c77312fabc16320
Darrel Guerrero <ddayy.guerrero@hotmail.com>
2016-11-02 22:58:34 -0400

    Create room scheduler template with jqWidgets

--- Layers/domain_layer/handler/app.go
+++ Layers/domain_layer/handler/app.go
@@ -10,6 +10,7 @@ func Home(rw http.ResponseWriter, req *http.Request) {
     templates := []string{
         filepath.Join("presentation_layer", "template", "menu.html"),
         filepath.Join("presentation_layer", "template", "footer.html"),
+        filepath.Join("presentation_layer", "template", "roomScheduler.html"),
         filepath.Join("presentation_layer", "template", "app.html"),
     }
 	t := (template.Must(template.ParseFiles(templates...)))
