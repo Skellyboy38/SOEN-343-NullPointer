d352a6a4b82382b42d641d5dbe6bd4516394a710
McGiver- <gkolampas@gmail.com>
2016-11-11 19:29:40 -0500

    fixed it so you dont need to start the db server yourself anymore.

--- Layers/controller.go
+++ Layers/controller.go
@@ -2,16 +2,23 @@ package main
 
 import (
 	"fmt"
+	"github.com/Skellyboy38/SOEN-343-NullPointer/Layers/domain_layer/handler"
+	"github.com/Skellyboy38/SOEN-343-NullPointer/Layers/domain_layer/mappers"
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
-	"github.com/Skellyboy38/SOEN-343-NullPointer/Layers/domain_layer/handler"
-	"github.com/Skellyboy38/SOEN-343-NullPointer/Layers/domain_layer/mappers"
+	"os/exec"
 )
 
 func main() {
 	fmt.Println("Application started")
 	router := mux.NewRouter()
+	err := startDb()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Db started")
 	mappers.Init()
 	mappers.InitUOW()
 	router.PathPrefix("/js/").Handler(http.StripPrefix("/js/", http.FileServer(http.Dir("./presentation_layer/js"))))
@@ -26,4 +33,13 @@ func main() {
 	router.HandleFunc("/getcookie", handler.GetCookie).Methods("GET")
 	http.Handle("/", router)
 	log.Fatal(http.ListenAndServe(":9000", nil))
+}
+
+func startDb() error {
+	path, err := exec.LookPath("pg_ctl.exe")
+	if err != nil {
+		return err
+	}
+	cmd := exec.Command(path, "-D", "data_source_layer/setup/registry", "start")
+	return cmd.Run()
 }
