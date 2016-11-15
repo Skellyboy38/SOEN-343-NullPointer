e8f9533c1904e14c647fdf168612e98b462f5903
McGiver- <gkolampas@gmail.com>
2016-11-15 11:58:04 -0500

    should work

--- Layers/domain_layer/tdg/reservationTdg.go
+++ Layers/domain_layer/tdg/reservationTdg.go
@@ -1,18 +1,18 @@
 package tdg
 
 import (
+	"errors"
 	"fmt"
 	"time"
-	"errors"
 )
 
 type ReservationTDG struct {
 	AbstractTDG AbstractTDG
 }
 
-func (r *ReservationTDG) Create(roomId, userId []int, startTime, endTime []time.Time) []int{
+func (r *ReservationTDG) Create(roomId, userId []int, startTime, endTime []time.Time) []int {
 	reservationIds := []int{}
-	for i,_ := range roomId{
+	for i, _ := range roomId {
 		res, err := DB.Exec("INSERT INTO reservation ( roomId, studentId, startTime, endTime)"+
 			"VALUES ($1, '$2', $3, '$4');",
 			roomId[i],
@@ -20,8 +20,8 @@ func (r *ReservationTDG) Create(roomId, userId []int, startTime, endTime []time.
 			startTime[i],
 			endTime[i])
 		fmt.Println(err)
-		id , err := res.LastInsertId()
-		reservationIds = append(reservationIds,int(id))
+		id, err := res.LastInsertId()
+		reservationIds = append(reservationIds, int(id))
 	}
 	return reservationIds
 }
@@ -60,71 +60,68 @@ func (r *ReservationTDG) Create(roomId, userId []int, startTime, endTime []time.
 //	return roomIds,studentIds,startTimes,endTimes, nil
 //}
 
-
 func (r *ReservationTDG) ReadByRoom(roomId int) ([]int, []int, []int, []time.Time, []time.Time, error) {
-	rows, err := DB.Query("SELECT * FROM reservation WHERE roomId=$1 ;",roomId)
-	if err != nil{
+	rows, err := DB.Query("SELECT * FROM reservation WHERE roomId=$1 ;", roomId)
+	if err != nil {
 		fmt.Println(err)
 	}
 	reservationIds := []int{}
 	roomIds := []int{}
 	studentIds := []int{}
 	startTimes := []time.Time{}
-	endTimes   := []time.Time{}
+	endTimes := []time.Time{}
 
 	var reservationId int
 	var studentId int
-	var startTime  time.Time
+	var startTime time.Time
 	var endTime time.Time
 
-
-	for rows.Next(){
-		err = rows.Scan(&reservationId,&roomId,&studentId,&startTime,&endTime)
-		if err != nil{
-			return reservationIds,roomIds,studentIds,startTimes,endTimes,
+	for rows.Next() {
+		err = rows.Scan(&reservationId, &roomId, &studentId, &startTime, &endTime)
+		if err != nil {
+			return reservationIds, roomIds, studentIds, startTimes, endTimes,
 				errors.New("Could not Scan Reservation by RoomId")
 		}
-		reservationIds = append(reservationIds,reservationId)
-		roomIds    = append(roomIds,roomId)
-		studentIds = append(studentIds,studentId)
-		startTimes = append(startTimes,startTime)
-		endTimes   = append(endTimes,endTime)
+		reservationIds = append(reservationIds, reservationId)
+		roomIds = append(roomIds, roomId)
+		studentIds = append(studentIds, studentId)
+		startTimes = append(startTimes, startTime)
+		endTimes = append(endTimes, endTime)
 	}
 
-	return reservationIds,roomIds,studentIds,startTimes,endTimes, nil
+	return reservationIds, roomIds, studentIds, startTimes, endTimes, nil
 }
 
 func (r *ReservationTDG) ReadByUser(roomId, userId int) ([]int, []int, []int, []time.Time, []time.Time, error) {
-	rows, err := DB.Query("SELECT * FROM reservation WHERE roomId=$1 and studentId=&2 ;",roomId,userId)
-	if err != nil{
+	rows, err := DB.Query("SELECT * FROM reservation WHERE roomId=$1 and studentId=$2 ;", roomId, userId)
+	if err != nil {
 		fmt.Println(err)
 	}
 	reservationIds := []int{}
 	roomIds := []int{}
 	studentIds := []int{}
 	startTimes := []time.Time{}
-	endTimes   := []time.Time{}
+	endTimes := []time.Time{}
 
 	var reservationId int
 	var studentId int
-	var startTime  time.Time
+	var startTime time.Time
 	var endTime time.Time
 
-
-	for rows.Next(){
-		err = rows.Scan(&reservationId,&roomId,&studentId,&startTime,&endTime)
-		if err != nil{
-			return reservationIds,roomIds,studentIds,startTimes,endTimes,
+	for rows.Next() {
+		err = rows.Scan(&reservationId, &roomId, &studentId, &startTime, &endTime)
+		if err != nil {
+			return reservationIds, roomIds, studentIds, startTimes, endTimes,
 				errors.New("Could not Scan Reservation by RoomId")
 		}
-		reservationIds = append(reservationIds,reservationId)
-		roomIds    = append(roomIds,roomId)
-		studentIds = append(studentIds,studentId)
-		startTimes = append(startTimes,startTime)
-		endTimes   = append(endTimes,endTime)
+		reservationIds = append(reservationIds, reservationId)
+		roomIds = append(roomIds, roomId)
+		studentIds = append(studentIds, studentId)
+		startTimes = append(startTimes, startTime)
+		endTimes = append(endTimes, endTime)
 	}
 
-	return reservationIds,roomIds,studentIds,startTimes,endTimes, nil
+	return reservationIds, roomIds, studentIds, startTimes, endTimes, nil
 }
 
 func (r *ReservationTDG) Update() {
