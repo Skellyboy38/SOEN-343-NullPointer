a8349cdb0ab57e436cf73d7637575edacbad5f95
Robert McAuley <Robert McAuley>
2016-11-21 21:48:19 -0500

    changed reservations entries into good values when init db

--- Layers/data_source_layer/setup/fillReservationTable.sql
+++ Layers/data_source_layer/setup/fillReservationTable.sql
@@ -1,7 +1,7 @@
-INSERT INTO reservation ( roomId, studentId, startTime, endTime) VALUES (1, 1111111, '2016-11-15 08:23:54', '2016-11-15 10:23:54');
-INSERT INTO reservation ( roomId, studentId, startTime, endTime) VALUES (1, 1111111, '2016-11-16 12:23:54', '2016-11-16 13:23:54');
-INSERT INTO reservation ( roomId, studentId, startTime, endTime) VALUES (2, 1111111, '2016-11-17 15:23:54', '2016-11-17 17:23:54');
-INSERT INTO reservation ( roomId, studentId, startTime, endTime) VALUES (2, 1111111, '2016-11-18 15:23:54', '2016-11-18 17:23:54');
-INSERT INTO reservation ( roomId, studentId, startTime, endTime) VALUES (2, 1111111, '2016-11-19 15:23:54', '2016-11-19 17:23:54');
-INSERT INTO reservation ( roomId, studentId, startTime, endTime) VALUES (1, 1111111, '2016-11-21 15:23:54', '2016-11-21 17:23:54');
-INSERT INTO reservation ( roomId, studentId, startTime, endTime) VALUES (1, 2222222, '2016-11-22 15:23:54', '2016-11-22 17:23:54');
+INSERT INTO reservation ( roomId, studentId, startTime, endTime) VALUES (1, 1111111, '2016-11-15 08:00:00', '2016-11-15 10:00:00');
+INSERT INTO reservation ( roomId, studentId, startTime, endTime) VALUES (1, 1111111, '2016-11-16 12:00:00', '2016-11-16 13:00:00');
+INSERT INTO reservation ( roomId, studentId, startTime, endTime) VALUES (2, 1111111, '2016-11-17 15:00:00', '2016-11-17 17:00:00');
+INSERT INTO reservation ( roomId, studentId, startTime, endTime) VALUES (2, 1111111, '2016-11-18 15:00:00', '2016-11-18 17:00:00');
+INSERT INTO reservation ( roomId, studentId, startTime, endTime) VALUES (2, 1111111, '2016-11-19 15:00:00', '2016-11-19 17:00:00');
+INSERT INTO reservation ( roomId, studentId, startTime, endTime) VALUES (1, 1111111, '2016-11-21 15:00:00', '2016-11-21 17:00:00');
+INSERT INTO reservation ( roomId, studentId, startTime, endTime) VALUES (1, 2222222, '2016-11-22 15:00:00', '2016-11-22 17:00:00');
